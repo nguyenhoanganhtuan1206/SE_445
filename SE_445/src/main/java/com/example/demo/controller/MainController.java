3e81bd082a2cd96eff904a998c284faf261e0a10
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-07-26 21:50:41 +0700

    push

--- SE_445/src/main/java/com/example/demo/controller/MainController.java
+++ SE_445/src/main/java/com/example/demo/controller/MainController.java
@@ -7,6 +7,7 @@ import com.example.demo.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class MainController {
 
     private static final String KHACH_HANG_PATH = "D:\\TichHop\\SE_445\\src\\main\\java\\com\\example\\demo\\data\\KhachHang.tsv";
