2bb9186205bd02be4c5d103929a51b36f411cbef
nguyenhoanganhtuan1206 <nguyenhanhtuan1206@gmail.com>
2022-08-10 22:19:21 +0700

    done

--- SE_445/src/main/java/com/example/demo/repository/TiemCamDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.models.TiemCamDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TiemCamDoRepository extends JpaRepository<TiemCamDo , String> {
-}
